0c584041cacd19213ebe5b4290876f49e8c5822e
mstrielnikov <makstr@ukr.net>
2022-04-11 13:31:59 +0300

    Refactor file structure

--- socket.go
+++ socket.go
@@ -5,6 +5,10 @@ import (
 	"net"
 )
 
+var (
+	BufferRead = make([]byte, 1024)
+)
+
 func GetAddr(listener net.Listener) string {
 	return listener.Addr().(*net.TCPAddr).IP.String()
 }
@@ -20,17 +24,17 @@ func ResolveMulticast(addr string) (*net.UDPAddr, error) {
 		return nil, err
 	}
 	if !multicastAddr.IP.IsMulticast() {
-		return nil, fmt.Errorf("The addr %s is not multicast", multicastAddr.IP.String())
+		return nil, fmt.Errorf("the addr %s is not multicast", multicastAddr.IP.String())
 	}
 	return multicastAddr, nil
 }
 
 func MulticastEcho(msg string, socket *net.UDPConn) {
 	_, err := socket.Write([]byte(msg))
-	ThenTerminate(LogError(err))
+	Ok(Log(err))
 }
 
 // func MulticastListen(socket *net.UDPConn, buffer []byte) {
 // 	_, src, err := socket.ReadFromUDP(buffer)
-// 	ThenTerminate(LogError(err))
+// 	Terminate(LogError(err))
 // }
